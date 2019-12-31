eb236014f79a10eaac35dd588ae5e208798feaf0
artsiom.kotau <Artsiom.Kotau@itechart-group.com>
2019-12-31 14:59:24 +0300

    Add api routes

---
+++ services/api-service/src/by/cash_exchange/api/api.clj
@@ -0,0 +1,19 @@
+(ns by.cash-exchange.api.api
+  (:require
+    [compojure.core :refer :all]
+    [compojure.route :as route]))
+
+(defroutes app-routes
+           (GET "api" [])
+           ; find best exchange rate. Return collection (address, coordinates, name, exchange rate, total sum)
+           ;Could be ordered by destination
+           ;by default returns 10 best
+           (GET "api/best" [])
+           ; find closest exchange office. Return collection (address, coordinates, name, exchange rate, total sum)
+           ;Could be ordered by total sum
+           ;by default returns 10 closest
+           (GET "api/closest" [])
+           ; find optimal - closest. Return collection (address, coordinates, name, exchange rate, total sum)
+           ;Could be ordered by total sum
+           ;by default returns 10 optimal
+           (GET "api/optimal" []))
