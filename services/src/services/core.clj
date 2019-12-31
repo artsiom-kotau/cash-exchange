c1c2d1ed324b6e6d3bd640173ca0d5b2b7fef3a3
artsiom.kotau <Artsiom.Kotau@itechart-group.com>
2019-12-31 12:08:43 +0300

    Init commit

---
+++ services/src/services/core.clj
@@ -0,0 +1,7 @@
+(ns services.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ services/test/services/core_test.clj
@@ -0,0 +1,7 @@
+(ns services.core-test
+  (:require [clojure.test :refer :all]
+            [services.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
