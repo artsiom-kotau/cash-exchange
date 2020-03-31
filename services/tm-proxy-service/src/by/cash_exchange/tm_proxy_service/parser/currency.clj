9a7d221fdce77f2c3f1a2ce3f84d3b571f62619c
artsiom.kotau <Artsiom.Kotau@itechart-group.com>
2020-03-31 17:35:30 +0300

    Add parser

---
+++ services/tm-proxy-service/src/by/cash_exchange/tm_proxy_service/parser/currency.clj
@@ -0,0 +1,14 @@
+(ns by.cash-exchange.tm-proxy-service.parser.currency)
+
+;;https://myfin.by/currency/gomel
+
+
+;; start main worker
+;; for each position save bank name, coordinates,  exchange rates
+;;
+;; what should be injected: data storage api, url
+
+
+(defn parse [storage currency-url]
+
+  )
